2f0455a86283b16d83c4cda710f20d0a4eb435de
Abhi04maurya <134590476+Abhi04maurya@users.noreply.github.com>
2025-05-14 09:34:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "3670291"
+api_hash = "1f55b2e9d972c3a5ac3f008c51ed7a4a"
+bot_token = "7545498255:AAE6-l7q9j76rC1qpT7_3G_QN_FstKtMMM4"
